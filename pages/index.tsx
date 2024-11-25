1cbdabb7856f8686af3516442031b5b2e7487180
weiyeli <weiyeli@amazon.com>
2024-11-25 16:32:17 +0800

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,52 @@
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+import {useEffect, useState} from "react";
+import {generateClient} from "aws-amplify/data";
+import type {Schema} from "@/amplify/data/resource";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const { user, signOut } = useAuthenticator();
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({id})
+    }
 
-  useEffect(() => {
-    listTodos();
-  }, []);
+    function listTodos() {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+    useEffect(() => {
+        listTodos();
+    }, []);
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    function createTodo() {
+        client.models.Todo.create({
+            content: window.prompt("Todo content"),
+        });
+    }
+
+    return (
+        <main>
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+                ))}
+            </ul>
+            <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br/>
+                <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
+                    Review next steps of this tutorial.
+                </a>
+            </div>
+
+            <button onClick={signOut}>Sign out</button>
+        </main>
+    );
 }
